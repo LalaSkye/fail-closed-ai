179aee2e02cbcffb3b1d227349ed482f54b80790
Ricky Jones <ricky.mcjones@gmail.com>
2026-05-07 06:18:14 +0100

    export NEO Guard chain receipt builder

--- docs/neo-guard/neo_guard/__init__.py
+++ docs/neo-guard/neo_guard/__init__.py
@@ -1,3 +1,4 @@
+from neo_guard.chain import build_commit_chain_receipt
 from neo_guard.guard import resolve_operator_standing
 from neo_guard.models import NeoGuardOutcome, NeoGuardRequest, NeoGuardResponse
 
@@ -5,5 +6,6 @@ __all__ = [
     "NeoGuardOutcome",
     "NeoGuardRequest",
     "NeoGuardResponse",
+    "build_commit_chain_receipt",
     "resolve_operator_standing",
 ]
