29f9d116bfc609ae64434443e00244e3851e95d7
Ricky Jones <ricky.mcjones@gmail.com>
2026-05-07 06:20:32 +0100

    export NEO Guard end-to-end flow

--- docs/neo-guard/neo_guard/__init__.py
+++ docs/neo-guard/neo_guard/__init__.py
@@ -1,11 +1,18 @@
 from neo_guard.chain import build_commit_chain_receipt
+from neo_guard.commit_gate import CommitDecision, CommitGateRequest, CommitGateResponse, resolve_commit
+from neo_guard.end_to_end import run_neo_to_commit_flow
 from neo_guard.guard import resolve_operator_standing
 from neo_guard.models import NeoGuardOutcome, NeoGuardRequest, NeoGuardResponse
 
 __all__ = [
+    "CommitDecision",
+    "CommitGateRequest",
+    "CommitGateResponse",
     "NeoGuardOutcome",
     "NeoGuardRequest",
     "NeoGuardResponse",
     "build_commit_chain_receipt",
+    "resolve_commit",
     "resolve_operator_standing",
+    "run_neo_to_commit_flow",
 ]
