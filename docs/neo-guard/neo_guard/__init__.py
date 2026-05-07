2bac2f20721eca5d60ddbcec27d47d47b9184a91
Ricky Jones <ricky.mcjones@gmail.com>
2026-05-07 06:15:58 +0100

    add NEO Guard package init

---
+++ docs/neo-guard/neo_guard/__init__.py
@@ -0,0 +1,9 @@
+from neo_guard.guard import resolve_operator_standing
+from neo_guard.models import NeoGuardOutcome, NeoGuardRequest, NeoGuardResponse
+
+__all__ = [
+    "NeoGuardOutcome",
+    "NeoGuardRequest",
+    "NeoGuardResponse",
+    "resolve_operator_standing",
+]
